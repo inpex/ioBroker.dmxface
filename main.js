74ef9ec819f88b97f2a638b476019a1063bc5b9d
dmxface <mail@dmxface.at>
2019-10-01 12:16:26 +0200

    active send

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// ADAPTER <--> IObroker
 const utils = require('@iobroker/adapter-core');
 var adapter  = utils.Adapter ('dmxface');
 
@@ -7,57 +7,87 @@ var adapter  = utils.Adapter ('dmxface');
 
 // DMXFACE CONNECTION
 var IPADR  = "0.0.0.0";
-var PORT = 5000;
+var PORT = 0;
 var TIMING = 1000;
-var DMX_CHANNELS_USED = 224;
+var DMX_CHANNELS_USED = 0;
 var INPORT =[];
 var BUSINPORT= [];
 var OUTPORT = [];
 var DMX=[];
 var AD_INPORT = [];
 
-var OBJID_REQUEST;
+var OBJID_REQUEST;  // OBJECT ID of TIMED DATA REQUEST
 
-// INTERNE
+// DMXface TCP Connection
 var net = require ('net');
 var client = new net.Socket();
 
-// CHECK FLAG ob ONLINBE
+// CHECK FLAG true when connection established and free of error
 var IS_ONLINE  = false;
 
 adapter.on ('ready',function (){
+	//Get the adapter configuration from IObroker
 	IPADR = adapter.config.ipaddress;
 	PORT = adapter.config.port;
 	TIMING = adapter.config.requesttiming; 
 	DMX_CHANNELS_USED = parseInt(adapter.config.lastdmxchannel);
-	//MAX LIMIT DMX CHANNELS, ist mit der ABFRAGE x14 auf 255 Channels begrenzt
-	if (DMX_CHANNELS_USED >255) {DMX_CHANNELS_USED = 255};
+	//LIMIT the number of DMX channels max. 224 usable with IObroker
+	if (DMX_CHANNELS_USED >224) {DMX_CHANNELS_USED = 224};
 	if (DMX_CHANNELS_USED <0) {DMX_CHANNELS_USED = 0};
 	adapter.config.lastdmxchannel = DMX_CHANNELS_USED;
 	adapter.log.info ('Connecting DMXface ' +IPADR + ' Port:' + PORT + '  Timing:' + TIMING + 'ms  DMXchannels:' + DMX_CHANNELS_USED);
 
+//OBJEKTE initialisieren entweder alle oder aus Config
+		var i;
+		for (i=1;i<=DMX_CHANNELS_USED;i++){
+			adapter.log.info (GetDMX(i));
+			adapter.setObjectNotExists (GetDMX(i),{
+				type:'state',
+					common:{name:'DMX channel'+i ,type:'number',role:'value',read:true,write:true},
+					native:{}
+			});
+		}
+		for (i=1;i<=16;i++){
+		adapter.setObjectNotExists (GetOUT(i),{
+			type:'state',
+				common:{name:'OUTPORT'+i,type:'boolean',role:' indicator',read:true,write:true},
+				native:{}
+		});		
+		}
+		for (i=1;i<=16;i++){
+		adapter.setObjectNotExists (GetIN(i),{
+			type:'state',
+				common:{name:'INPORT'+i,type:'boolean',role:' indicator',read:true,write:false},
+				native:{}
+		});		
+		}
 
-// Verbinden des Client
+// Connect the DMXface server
 	CONNECT_CLIENT();
-//ABFRAGE PROZESS initialisieren
-	OBJID_REQUEST = setInterval (CLIENT_REQUEST,TIMING);
+// Initialize the request process
+	//OBJID_REQUEST = setInterval (CLIENT_REQUEST,TIMING);
 	}
 	);
 
+
+// Adapter termination by IObroker
 adapter.on ('unload',function (callback){
 	adapter.log.info ('DMXface close connection, cancel service');
-	clearInterval (OBJID_REQUEST);
+	//clearInterval (OBJID_REQUEST);
 	CLIENT.close;
 	callback;
 	}
 	);
 
 
+//State Change	
+adapter.on ('statechange',function (id,state){
+	adapter.log.info ("ID:" + id + " STATE:" + state);
 	
+});
 
 
-
-//Client verbinden  
+//Connect the client
 function CONNECT_CLIENT () {
 	//adapter.log.info('DMXface CONNECTING');
 	IS_ONLINE = false;
@@ -65,7 +95,7 @@ function CONNECT_CLIENT () {
 	client.connect (PORT,IPADR,CBclientCONNECT);
 }
 
-//Event CLIENT CONNECTED
+//CLIENT CONNECTED callback
 function CBclientCONNECT () {
 	// Handler
 	client.on ('data',CBclientRECEIVE);
@@ -76,10 +106,10 @@ function CBclientCONNECT () {
 }
 
 	
-//CLIENT ABFRAGE 
+//todo CLIENT ABFRAGE nur noch für AD PORTS mit allem drum und dran
 function CLIENT_REQUEST	(){
 	if (IS_ONLINE = true) {
-		//PORTS und DMX Kanäle bis zu 224
+		//nur die AD Ports 
 		var WDATA = Buffer.from ([1,2,0,20,224,3]);  // Nur neues Format 16 Bit STX 0x01, LEN LOW, LEN HIGH, x DATBYTES , ETX 0x03
 		WDATA[4] = DMX_CHANNELS_USED;
 		client.write (WDATA);  // STX , 2 BYTE, CHAR 14 = ABFRAGE / 32 Channels, ETX , Binär ausgeben
@@ -95,110 +125,79 @@ function CBclientERROR(Error) {
 
 
 //------------------------------------------------------ RX DATA PROCESSING --------------------------------------------------------------------
-// RX DATEN verarbeiten 
+// Prozessing the received ACTIVE SEND data from DMXface 
 function CBclientRECEIVE(RXdata) {
-	var CMD_REFLECT = 0;
-	var CMD_OPTION = 0;
-	var DATA_POSITION = 0;
-	var DATA_LEN = 0;
-	//console.log ('ENTER RX' );
-	if (RXdata.length < 6) {return;}			// Minimum Length of response STX 0x01 ,LEN LOW, LEN HIGH, 0 Bytes DATA ,CMD_REFLECT,CMD_OPTION,ETX	console.log ('DATA2:' + RXdata[2]);
+	if (RXdata.length < 3) {return;}			// Minimum Length of response ist start 0xF0, Signature 0xnn and at least one data byte 
 	
-	if (RXdata[0] = 0x01) {						// CHECK START BYTE 0x01 qwith 16 Bit Length or 0x02 with 8 Bit Length
-		DATA_POSITION = 3;
-		DATA_LEN =  (RXdata[1] + (RXdata[2]<<8));  //UNBEDINGT in Klammern sonst wird falsch gerechnet
+	if (RXdata[0] != 0xF0) {					// CHECK START BYTE =0xF0
+		return;
 	}
-	if (DATA_POSITION == 0) {return;} 			//nicht gesetzt da ungültiges Startzeichen
+	var i;
+	var x;	
 	
-	
-	//COMMAND REFLECTION und RX Zurordnung	
-	CMD_REFLECT = (RXdata[RXdata.length-3]);	// Extract Command Reflection must be Value 
-	if (CMD_REFLECT <= 0x80) {return;}			// Must be Value > 0x80
-	CMD_REFLECT = (CMD_REFLECT & 0x7F);    		// Extract the Source command 
-	CMD_OPTION = RXdata[RXdata.length-2];		// Extract the option byte
-	
-	
-	switch (CMD_REFLECT) {
-		case 0x14:   // KOMMANDO zur ABFRAGE aller INPORTS und DMX bis zu 255 Channels in einem Request (siehe Communication Manual) 
-			//OUTPORT
-			var i;
-			var x;
-			
-			for (i=0;i<8;i++){
-				x=2**i;					//2 hoch i
-				
-				//OUTPORT 1-8 steht auf Byte 4 (offset 3)
-				if (x & RXdata[DATA_POSITION+3]) {
-					OUTPORT[i] = true;
-					} else { 
-					OUTPORT[i] = false;
-				}
-				
-				//OUTPORT 9-16 steht auf Byte 3 (offset 2)
-				if (x & RXdata[DATA_POSITION+2]) {
-					OUTPORT[i+8] = true;
-					} else { 
-					OUTPORT[i+8] = false;
-				}
-				
-				//INPORT IN1-8 auf Byte 16 (Offset 15) INPORT 9-16 auf Byte 15 ; (Inport 17-24 auf Byte 14)
-			    if (x & RXdata[DATA_POSITION+15]) {
-					INPORT[i] = true;
-					} else { 
-					INPORT[i] = false;
-				}
-								
-				//INPORT IN9-16 auf Byte 15 (Offset 14) 
-			    if (x & RXdata[DATA_POSITION+14]) {
-					INPORT[i+8] = true;
-					} else { 
-					INPORT[i+8] = false;
-				}
-				//nicht implementiert INPORT IN17-24 auf Byte 14 (Offset 13) 
-			
-			    //BUS 1.x auf BYTE 12 
-				if (x & RXdata[DATA_POSITION+11]) {
-					BUSINPORT[i] = true;
-					} else { 
-					BUSINPORT[i] = false;
-				}
-				 //BUS 2.x auf BYTE 11 
-				if (x & RXdata[DATA_POSITION+10]) {
-					BUSINPORT[i+8] = true;
-					} else { 
-					BUSINPORT[i+8] = false;
-				}
-				 //BUS 3.x auf BYTE 10 
-				if (x & RXdata[DATA_POSITION+9]) {
-					BUSINPORT[i+16] = true;
-					} else { 
-					BUSINPORT[i+16] = false;
-				}
-				//BUS 4.x auf BYTE 9 
-				if (x & RXdata[DATA_POSITION+8]) {
-					BUSINPORT[i+24] = true;
-					} else { 
-					BUSINPORT[i+24] = false;
-				}
-			}
-			//DMX CHANNELS 1-n auf Byte 33 (Offset 32)
-			for (i=0;i < DMX_CHANNELS_USED;i++){
-			DMX[i] = RXdata[i+32+DATA_POSITION]	
-			}
-			
-			//AD_INPORTs 1-16 ab BYTE 17 (offset 16)
-			for (i=0;i < 16;i++){
-			AD_INPORT[i] = RXdata[i+16+DATA_POSITION]		
-			}
-		break;
+	switch (RXdata[1]) {
+		case 0x01:			// IR RECEIVE 
+			break;
 		
-		//case nächster:
+		case 0x02:   		//RECEIVING INPORT STATE INFO //9 Bytes RX length
+			if (RXdata.length == 9){    
+				var ONOFF = false;
+				x =1;
+				for (i=1;i<0x81;i*=2){
+					if (i & RXdata[8]){ONOFF = true;} else {ONOFF = false;}
+					adapter.setState(GetIN(x),ONOFF);
+					if (i & RXdata[7]){ONOFF = true;} else {ONOFF = false;}
+					adapter.setState(GetIN(x+8),ONOFF);
+					x+=1;
+				}
+			}
+			break;
 		
+		case 0x04:	//OUTPORT  //5 Bytes RX length
+			var ONOFF = false;
+			if (RXdata.length == 5){   
+				x =1;
+				for (i=1;i<0x81;i*=2){
+					if (i & RXdata[4]){ONOFF = true;} else {ONOFF = false;}
+					adapter.setState(GetOUT(x),ONOFF);
+					if (i & RXdata[3]){ONOFF = true;} else {ONOFF = false;}
+					adapter.setState(GetOUT(x+8),ONOFF);
+					x+=1;
+				}
+			}
+			break;
+			
+		case 0xFF:	//DMX OUT DATA
+			var USED_DXMOUT = (RXdata.length-2);
+			if (DMX_CHANNELS_USED < USED_DXMOUT) {
+				USED_DXMOUT = DMX_CHANNELS_USED;
+				}
+			
+			for (i=1;i <= USED_DXMOUT;i++){
+				adapter.setState(GetDMX(i),RXdata[i+1]);				
+				}
+			break;
+			
 		default:
 			return;
 			break;
 	}
 
 	
+}
+
+
+function GetDMX (number){
+	if (number <10) {return 'DMX00'+number;}
+	if (number <100) {return 'DMX0'+number;}
+	return 'DMX'+number;
+}
+function GetOUT (number){
+	if (number <10) {return 'OUTPORT0'+number;}
+	return 'OUTPORT'+number;
+}
+function GetIN (number){
+	if (number <10) {return 'INPORT0'+number;}
+	return 'INPORT'+number;
 }
 

2aa5096f4a976e2a63ab8ce31d023800856affd4
Scheffi <kevin@cortellini.de>
2021-06-02 14:18:44 +0200

    test

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 //Implements min max tracking of addition reads
 
 //REV 1.0.2 First release
-const adaptername = "dmxface"
+const adaptername = "dmxface2"
 
 const utils = require('@iobroker/adapter-core');
 var adapter  = utils.Adapter (adaptername);
